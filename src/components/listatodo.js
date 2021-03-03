994c9dc8a87741b6e11f9f7caee9e699d344454e
Erich <erich_slip@hotmail.com>
2021-03-02 23:37:11 -0300

    todo list made with react

---
+++ src/components/listatodo.js
@@ -0,0 +1,19 @@
+import React from "react";
+//import Components
+import Todo from "./todo";
+
+
+const TodoList = ({ todos, setTodos, filteredTodos }) => {
+  //console.log(todos);
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo setTodos={setTodos} todos={todos} key={todo.id} todo={todo} text={todo.text}/>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
